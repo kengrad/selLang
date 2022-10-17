51bbc5aa79ae0dd113a32eac9ef7a2cf9234d4a1
kengrad <xkenig@gmail.com>
2022-10-17 23:45:09 +0400

    lang

--- conftest.py
+++ conftest.py
@@ -5,39 +5,18 @@ from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
-                     help="Choose lanuage: ru or es")
+                     help="Choose language: ru, es or fr")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     options = Options()
     language = request.config.getoption("language")
-    if language == "ru":
+    if language == "ru" or language == "es" or language == "fr":
         options.add_experimental_option('prefs', {'intl.accept_languages': language})
-        print("\nstart russian language for test..")
-        browser = webdriver.Chrome(options=options)
-    elif language == "es":
-        options.add_experimental_option('prefs', {'intl.accept_languages': language})
-        print("\nstart espaniol language for test..")
         browser = webdriver.Chrome(options=options)
     else:
-        raise pytest.UsageError("--language should be ru or es")
+        raise pytest.UsageError("--language should be ru, es or fr")
     yield browser
     print("\nquit browser..")
     browser.quit()
-
-# @pytest.fixture(scope="function")
-# def browser(request):
-#     browser_name = request.config.getoption("browser_name")
-#     browser = None
-#     if browser_name == "chrome":
-#         print("\nstart chrome browser for test..")
-#         browser = webdriver.Chrome()
-#     elif browser_name == "firefox":
-#         print("\nstart firefox browser for test..")
-#         browser = webdriver.Firefox()
-#     else:
-#         raise pytest.UsageError("--browser_name should be chrome or firefox")
-#     yield browser
-#     print("\nquit browser..")
-#     browser.quit()
--- test_items.py
+++ test_items.py
@@ -7,8 +7,7 @@ import time
 def test_lang(browser):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(5)
-    button = WebDriverWait(browser, 5).until(
+    time.sleep(30)
+    assert WebDriverWait(browser, 5).until(
         EC.element_to_be_clickable((By.ID, "add_to_basket_form"))
     )
-    browser.implicitly_wait(15)
